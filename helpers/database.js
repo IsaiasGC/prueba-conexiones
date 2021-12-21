1b4787082ee73fcd6a04e16b7ce8d4e4f7c45238
Isaias Gonzalez <igonzalez@gobdigital.com>
2021-12-20 18:00:17 -0600

    [UPDATE][db,ws] Se modifico la prueba con variables de entorno

--- helpers/database.js
+++ helpers/database.js
@@ -1,9 +1,14 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-	dialect: process.env.DB_DIALECT,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  }
+);
 
 module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const path = require("path");
+const path = require('path');
 
-if (process.env.NODE_ENV !== "production") {
-	require("dotenv").config({ path: path.resolve(process.cwd(), ".env.local") });
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line import/no-extraneous-dependencies,global-require
+  require('dotenv').config({ path: path.resolve(process.cwd(), '.env.local') });
 }
 
-const db = require("./helpers/database");
-const mqtt = require("./helpers/websocket");
+const db = require('./helpers/database');
+const mqtt = require('./helpers/websocket');
+
+const { log, error } = console;
 
 // Probando coniexion a la BD
 db.authenticate()
-	.then(() => {
-		console.log("Database connection has been established successfully.");
-		db.close();
-	})
-	.catch((err) => {
-		console.error("Unable to connect to the database:", err);
-	});
+  .then(() => {
+    log('Database connection has been established successfully.');
+    db.close();
+  })
+  .catch((err) => {
+    error('Unable to connect to the database:', err);
+  });
 
 // Probando conexion MQTT
-let client = mqtt.getClient();
+const client = mqtt.getClient();
 mqtt.subscribe(client);
 mqtt.onMessage(client, () => mqtt.closeConnection(client));
